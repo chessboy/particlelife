cfb5bc4a5b801e7e65f39ea66c05b969d866e22c
Rob Silverman <chessboy@mac.com>
2025-02-14 15:34:57 -0500

    Initial Commit

---
+++ particlelife/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  particlelife
+//
+//  Created by Rob Silverman on 2/14/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ particlelifeTests/particlelifeTests.swift
@@ -0,0 +1,17 @@
+//
+//  particlelifeTests.swift
+//  particlelifeTests
+//
+//  Created by Rob Silverman on 2/14/25.
+//
+
+import Testing
+@testable import particlelife
+
+struct particlelifeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
